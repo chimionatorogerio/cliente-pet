1b9b3bda356a76f5b1b11e996266ac33fe23559a
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-02 18:52:09 -0300

    implementa domínio de negócio

---
+++ src/main/java/br/com/petz/cliente_pet/cliente/domain/Cliente.java
@@ -0,0 +1,57 @@
+package br.com.petz.cliente_pet.cliente.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class Cliente {
+    @Id
+    private UUID idCliente;
+    @NotBlank
+    private String nomeCompleto;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String celular;
+    private String telefone;
+    private Sexo sexo;
+    @NotNull
+    private LocalDate dataNascimento;
+    @NotNull
+    private Boolean aceitaTermos;
+    @CPF
+    private String cpf;
+
+    private LocalDateTime dataHoraDoCadastro;
+    private LocalDateTime dataHoraDaUltimaAlteracao;
+
+    public Cliente(@NotBlank String nomeCompleto, @NotBlank @Email String email, @NotBlank String celular,
+                   String telefone, Sexo sexo, @NotNull LocalDate dataNascimento,
+                   @NotNull Boolean aceitaTermos, @CPF String cpf) {
+        this.idCliente = UUID.randomUUID();
+        this.nomeCompleto = nomeCompleto;
+        this.email = email;
+        this.celular = celular;
+        this.telefone = telefone;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+        this.aceitaTermos = aceitaTermos;
+        this.cpf = cpf;
+        this.dataHoraDoCadastro = LocalDateTime.now();
+
+    }
+}
