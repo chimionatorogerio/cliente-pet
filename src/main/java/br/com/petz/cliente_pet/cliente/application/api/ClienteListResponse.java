38f7d01c8e507c89fb336d3a2a0cd3e1dfd8d6cc
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-03 17:47:02 -0300

    implementa getTodosClientes, teste ok

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteListResponse.java
@@ -1,10 +1,13 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
 import br.com.petz.cliente_pet.cliente.domain.Cliente;
+import lombok.Value;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Value
 public class ClienteListResponse {
     private UUID idCliente;
     private String nomeCompleto;
@@ -13,6 +16,19 @@ public class ClienteListResponse {
     private String email;
 
     public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-        return null;
+        return clientes.stream()
+                .map(ClienteListResponse::new)
+                .collect(Collectors.toList());
+    }
+    //.map(c -> new ClienteListResponse(c))
+    //    é a mesma coisa que
+    //    .map(ClienteListResponse::new)
+
+    private ClienteListResponse(Cliente cliente) {
+        this.idCliente = cliente.getIdCliente();
+        this.nomeCompleto = cliente.getNomeCompleto();
+        this.cpf = cliente.getCpf();
+        this.celular = cliente.getCelular();
+        this.email = cliente.getEmail();
     }
 }
