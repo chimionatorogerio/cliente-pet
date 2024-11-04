4c82b86abd6077e514c250a7119eeab4a24b4970
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-04 16:43:41 -0300

    patch: criada classe clientealteracaorequest so com os campos q podem ser alterados - criado patch no clienteapi - criado patch no clientecontroller so com log sem metodo - postman criada request patch - teste ok 204 no content

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteAPI.java
@@ -26,5 +26,10 @@ public interface ClienteAPI {
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     void deletaClienteAtravesId(@PathVariable UUID idCliente);
 
+    @PatchMapping(value = "/{idCliente}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    void patchAlteraCliente(@PathVariable UUID idCliente,
+                            @Valid @RequestBody ClienteAlteracaoRequest clienteAlteracaoRequest);
+
 
 }
