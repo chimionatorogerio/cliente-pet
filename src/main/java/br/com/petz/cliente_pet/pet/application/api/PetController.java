e25651f006aa4f0157eb566ffed66202f6312e78
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-05 16:42:54 -0300

    implementa petService.criaPet na PetController - cria interface PetService - cria metodo criaPet na PetService

--- src/main/java/br/com/petz/cliente_pet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/cliente_pet/pet/application/api/PetController.java
@@ -1,4 +1,7 @@
 package br.com.petz.cliente_pet.pet.application.api;
+import br.com.petz.cliente_pet.pet.application.service.PetService;
+import br.com.petz.cliente_pet.pet.domain.Pet;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -6,12 +9,15 @@ import java.util.UUID;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class PetController implements PetAPI {
+    private final PetService petService;
     @Override
     public PetResponse postPet(UUID idCliente, PetRequest petRequest) {
         log.info("[inicia] PetController - postPet");
         log.info("[idCliente] {}", idCliente);
+        PetResponse petCriado = petService.criaPet(idCliente, petRequest);
         log.info("[finaliza] PetController - postPet");
-        return null;
+        return petCriado;
     }
 }
