4397e00896eef7286e7e6ea24a2da8641fc1cc63
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-07 11:37:29 -0300

    Inicia método GET buscar um pet do cliente com id Implanta camada de api: PetAPI: foi criado o getPetDoClienteComId PetClienteDetalhadoResponse: foi criada essa classe com os campos

--- src/main/java/br/com/petz/cliente_pet/pet/application/api/PetAPI.java
+++ src/main/java/br/com/petz/cliente_pet/pet/application/api/PetAPI.java
@@ -1,5 +1,6 @@
 package br.com.petz.cliente_pet.pet.application.api;
 
+import br.com.petz.cliente_pet.cliente.application.api.ClienteDetalhadoResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/v1/cliente/{idCliente}/pet")
 public interface PetAPI {
+
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     PetResponse postPet(@PathVariable UUID idCliente,
@@ -19,6 +21,9 @@ public interface PetAPI {
     @ResponseStatus(code = HttpStatus.OK)
     List<PetClienteListResponse> getPetsDoClienteComId(@PathVariable UUID idCliente);
 
+    @GetMapping(value = "/{idPet}")
+    @ResponseStatus(code = HttpStatus.OK)
+    PetClienteDetalhadoResponse getPetDoClienteComId(@PathVariable UUID idCliente, @PathVariable UUID idPet);
 
 
 }
